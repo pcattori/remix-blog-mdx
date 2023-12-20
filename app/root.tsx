32aa0a1e5cb5db69dffe983f1e05ce3ab98744e0
Pedro Cattori <pcattori@gmail.com>
2023-12-20 10:28:23 -0600

    tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "~/tailwind.css";
+
 export default function App() {
   return (
     <html lang="en">
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{ts,tsx}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
