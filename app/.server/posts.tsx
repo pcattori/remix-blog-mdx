2da36ef7bb451a03a74b45385dde3bd787f8d783
Pedro Cattori <pcattori@gmail.com>
2023-12-23 15:43:00 -0600

    refactor

--- app/.server/posts.tsx
+++ app/.server/posts.tsx
@@ -10,34 +10,39 @@ export type PostMeta = {
   frontmatter: Frontmatter;
 };
 
-const slug = (filename: string) =>
-  "/" +
-  filename
-    .replace("../routes/", "")
-    .replace(/\.mdx$/, "")
-    .replace(/\./, "/");
-
 export const getPosts = (): PostMeta[] => {
   const modules = import.meta.glob<{ frontmatter: Frontmatter }>(
     "../routes/blog.*.mdx",
     { eager: true }
   );
-  const posts = Object.entries(modules).map(([file, post]) => ({
-    slug: slug(file),
-    frontmatter: post.frontmatter,
-  }));
-  posts.sort((a, b) => {
-    if (a.frontmatter.published < b.frontmatter.published) {
-      return 1;
-    } else if (a.frontmatter.published > b.frontmatter.published) {
-      return -1;
-    } else {
-      return 0;
-    }
+  const posts = Object.entries(modules).map(([file, post]) => {
+    let slug =
+      "/" +
+      file
+        .replace("../routes/", "")
+        .replace(/\.mdx$/, "")
+        .replace(/\./, "/");
+    return {
+      slug,
+      frontmatter: post.frontmatter,
+    };
   });
-  return posts;
+  return sortBy(posts, (post) => post.frontmatter.published, "desc");
 };
 
-export const getFeaturedPosts = (): PostMeta[] => {
-  return getPosts().filter((post) => post.frontmatter.featured);
-};
+function sortBy<T>(
+  arr: T[],
+  key: (item: T) => any,
+  dir: "asc" | "desc" = "asc"
+) {
+  return arr.sort((a, b) => {
+    const res = compare(key(a), key(b));
+    return dir === "asc" ? res : -res;
+  });
+}
+
+function compare<T>(a: T, b: T): number {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,13 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-import { getFeaturedPosts } from "~/.server/posts";
+import { getPosts } from "~/.server/posts";
 import { Post } from "~/components/post";
 
-export const loader = () => json(getFeaturedPosts());
+export const loader = () => {
+  const posts = getPosts();
+  return json(posts.filter((post) => post.frontmatter.featured));
+};
 
 export default function Index() {
   const featuredPosts = useLoaderData<typeof loader>();
